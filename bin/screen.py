8c3965981d938a18a3e1c3297b19f256de89f943
Ficles <ben5theboss81125@gmail.com>
2025-04-03 01:14:30 +1300

    Base screen functionality

--- bin/screen.py
+++ bin/screen.py
@@ -1,2 +1,23 @@
+import pygame
+
 class Screen():
-    test = 1
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.sprites = {}
+        self.background = (0, 0, 0)
+    
+    def load_sprite(self, name, image_path):
+        image = pygame.image.load(image_path)
+        self.sprites[name] = image
+
+    def show_sprite(self, name, pos):
+        image = self.sprites[name]
+        blit_pos = (0, 0)
+        blit_pos[0] = pos[0] + (image.get_width() / 2)
+        blit_pos[1] = pos[1] + (image.get_height()/ 2)
+        self.screen.blit(image, blit_pos)
+
+    def update(self):
+        pygame.display.flip(self.screen)
+        self.screen.fill(self.background)
